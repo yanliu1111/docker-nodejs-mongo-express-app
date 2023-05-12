74b8c061e826e06bb7eb1eaff4467091ff197e76
yanliu1111 <liuyan0504@gmail.com>
2023-05-11 23:19:02 -0600

    feat: change MongoClient to mongoose, w Dani

--- app/server.js
+++ app/server.js
@@ -1,7 +1,33 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import fs from "fs";
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 let app = express();
+
+const db = await mongoose.connect(
+  "mongodb://admin:password@localhost:27016/my-db?authSource=admin",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const UserSchema = new db.Schema({
+  userid: Number,
+  name: String,
+  email: String,
+  interests: String,
+});
+const User = db.model("User", UserSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -11,6 +37,29 @@ app.get("/profile-picture", function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, { "Content-Type": "image/jpg" });
   res.end(img, "binary");
+});
+
+let databaseName = "my-db";
+let query = { userid: 1 };
+
+app.post("/update-profile", function (req, res) {
+  let userObj = req.body;
+  console.log("connected to the mongodb line32");
+
+  userObj["userid"] = 1;
+  User.updateOne(query, userObj, { upsert: true }).then((result) => {
+    console.log("successfully updated the profile");
+    res.send("successfully updated the profile");
+  });
+});
+
+app.get("/get-profile", function (req, res) {
+  let response = res;
+  console.log("successfully connected to line58");
+  User.findOne(query).then((result) => {
+    console.log("successfully retrieved the profile");
+    response.send(result);
+  });
 });
 
 app.listen(3000, function () {
